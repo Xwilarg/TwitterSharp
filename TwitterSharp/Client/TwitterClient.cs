b03929c0f057437a8f965c73410d0e4ff49bb638
Xwilarg <xwilarg@yahoo.fr>
2021-05-04 01:36:42 +0200

    Add GetTweetStreamInfoAsync

--- TwitterSharp/Client/TwitterClient.cs
+++ TwitterSharp/Client/TwitterClient.cs
@@ -16,6 +16,7 @@ namespace TwitterSharp.Client
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
         }
 
+        #region TweetSearch
         public async Task<Tweet[]> GetTweetsByIdsAsync(params string[] ids)
         {
             var str = await _httpClient.GetStringAsync(_baseUrl + "tweets?ids=" + string.Join(",", ids.Select(x => HttpUtility.HtmlEncode(x))));
@@ -27,12 +28,23 @@ namespace TwitterSharp.Client
             var str = await _httpClient.GetStringAsync(_baseUrl + "users/" + HttpUtility.HtmlEncode(userId) + "/tweets");
             return JsonSerializer.Deserialize<Answer<Tweet[]>>(str, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }).Data;
         }
+        #endregion TweetSearch
 
+        #region TweetStream
+        public async Task<StreamInfo[]> GetTweetStreamInfoAsync()
+        {
+            var str = await _httpClient.GetStringAsync(_baseUrl + "tweets/search/stream/rules");
+            return JsonSerializer.Deserialize<Answer<StreamInfo[]>>(str, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }).Data;
+        }
+        #endregion TweetStream
+
+        #region UserSearch
         public async Task<User[]> GetUsersAsync(params string[] usernames)
         {
             var str = await _httpClient.GetStringAsync(_baseUrl + "users/by?usernames=" + string.Join(",", usernames.Select(x => HttpUtility.HtmlEncode(x))));
             return JsonSerializer.Deserialize<Answer<User[]>>(str, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }).Data;
         }
+        #endregion UserSearch
 
         private const string _baseUrl = "https://api.twitter.com/2/";
 
---
+++ TwitterSharp/Response/StreamInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TwitterSharp.Response
+{
+    public struct StreamInfo : IEquatable<StreamInfo>
+    {
+        public string Id { init; get; }
+        public string Value { init; get; }
+
+        public override bool Equals(object obj)
+            => obj is StreamInfo t && t.Id == Id;
+
+        public bool Equals(StreamInfo other)
+            => other.Id == Id;
+
+        public override int GetHashCode()
+            => Id.GetHashCode();
+
+        public static bool operator ==(StreamInfo left, StreamInfo right)
+            => left.Id == right.Id;
+
+        public static bool operator !=(StreamInfo left, StreamInfo right)
+            => left.Id != right.Id;
+    }
+}
