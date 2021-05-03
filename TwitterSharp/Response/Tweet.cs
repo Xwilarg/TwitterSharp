50ad9e0e3f5a2284bb83af05833747f45aca4f02
Xwilarg <xwilarg@yahoo.fr>
2021-05-04 00:24:58 +0200

    Improve Tweet struct

--- TwitterSharp/Response/Tweet.cs
+++ TwitterSharp/Response/Tweet.cs
@@ -2,7 +2,7 @@
 
 namespace TwitterSharp.Response
 {
-    public struct Tweet
+    public struct Tweet : IEquatable<Tweet>
     {
         public string Id { init; get; }
         public string Text { init;  get; }
@@ -10,7 +10,16 @@ namespace TwitterSharp.Response
         public override bool Equals(object obj)
             => obj is Tweet t && t.Id == Id;
 
+        public bool Equals(Tweet other)
+            => other.Id == Id;
+
         public override int GetHashCode()
             => Id.GetHashCode();
+
+        public static bool operator ==(Tweet left, Tweet right)
+            => left.Id == right.Id;
+
+        public static bool operator !=(Tweet left, Tweet right)
+            => left.Id != right.Id;
     }
 }
