efceb6677f144e52a64255b9ab9e59a2c9c51110
Xwilarg <xwilarg@yahoo.fr>
2021-05-08 01:46:39 +0200

    Add enum documentation

--- TwitterSharp/Request/AdvancedSearch/TweetOption.cs
+++ TwitterSharp/Request/AdvancedSearch/TweetOption.cs
@@ -11,15 +11,54 @@
         // organic_metrics
         // promoted_metrics
 
+        /// <summary>
+        /// The ID of the tweet this one is replying to
+        /// </summary>
         Conversation_Id,
+
+        /// <summary>
+        /// Datetime when the tweet was crated
+        /// </summary>
         Created_At,
+
+        /// <summary>
+        /// Entities in the tweet
+        /// </summary>
         Entities,
+
+        /// <summary>
+        /// In the case of a reply, will contains the original user id
+        /// </summary>
         In_Reply_To_User_Id,
+
+        /// <summary>
+        /// Language of the tweet, detect by Twitter
+        /// </summary>
         Lang,
+
+        /// <summary>
+        /// If contains a link, indicate if it might lead to a sensitive content
+        /// </summary>
         Possibly_Sensitive,
+
+        /// <summary>
+        /// Contains the following information: number of likes, number of retweets, number of replies, number of quotes 
+        /// </summary>
         Public_Metrics,
+
+        /// <summary>
+        /// Number of tweet this one reference
+        /// </summary>
         Referenced_Tweets,
+
+        /// <summary>
+        /// Show who can reply to this tweet
+        /// </summary>
         Reply_Settings,
+
+        /// <summary>
+        /// Name of the app this tweet was sent from
+        /// </summary>
         Source
     }
 }
