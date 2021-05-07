eaf940e87117fd4394d2ae55ec07d9786bacc438
Xwilarg <xwilarg@yahoo.fr>
2021-05-07 17:46:10 +0200

    Add more tweet info

---
+++ TwitterSharp/Request/AdvancedSearch/TweetOption.cs
@@ -0,0 +1,26 @@
+﻿namespace TwitterSharp.Request.AdvancedSearch
+{
+    // https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+    public enum TweetOption
+    {
+        // TODO:
+        // attachments
+        // context_annotations
+        // geo
+        // non_public_metrics
+        // organic_metrics
+        // promoted_metrics
+        // source
+
+        Conversation_Id,
+        Created_At,
+        Entities,
+        In_Reply_To_User_Id,
+        Lang,
+        Possibly_Sensitive,
+        Public_Metrics,
+        Referenced_Tweets,
+        Reply_Settings,
+        Source
+    }
+}
