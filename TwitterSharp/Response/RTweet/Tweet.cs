92ac0de8706d4e86a2983112e1b4b256e30aabd2
Xwilarg <xwilarg@yahoo.fr>
2022-01-05 12:28:28 +0100

    Add comments

--- TwitterSharp/Response/RTweet/Tweet.cs
+++ TwitterSharp/Response/RTweet/Tweet.cs
@@ -9,19 +9,61 @@ namespace TwitterSharp.Response.RTweet
 {
     public class Tweet : IEquatable<Tweet>, IHaveAuthor, IHaveMedia
     {
+        /// <summary>
+        /// Unique identifier of the tweet
+        /// </summary>
         public string Id { init; get; }
+        /// <summary>
+        /// Text of the tweet
+        /// </summary>
         public string Text { init; get; }
+        /// <summary>
+        /// ID of the user who created the tweet
+        /// </summary>
         public string AuthorId { init; get; }
+        /// <summary>
+        /// The ID of the tweet that the conversation is from
+        /// </summary>
         public string ConversationId { init; get; }
+        /// <summary>
+        /// When the tweet was created
+        /// </summary>
         public DateTime? CreatedAt { init; get; }
+        /// <summary>
+        /// Information about special entities in the tweet
+        /// </summary>
         public Entities Entities { init; get; }
+        /// <summary>
+        /// ID of the user to whom the tweet is replying
+        /// </summary>
         public string InReplyToUserId { init; get; }
+        /// <summary>
+        /// Language of the tweet (detected by Twitter)
+        /// </summary>
         public string Lang { init; get; }
+        /// <summary>
+        /// If the tweet may contain sensitive information
+        /// </summary>
         public bool? PossiblySensitive { init; get; }
+        /// <summary>
+        /// Public metrics of the tweet
+        /// </summary>
         public TweetPublicMetrics PublicMetrics { init; get; }
+        /// <summary>
+        /// Name of the device that was used to make the tweet
+        /// </summary>
         public string Source { init; get; }
+        /// <summary>
+        /// Tweet that were referenced by this one
+        /// </summary>
         public ReferencedTweet[] ReferencedTweets { init; get; }
+        /// <summary>
+        /// Who can reply to this tweet
+        /// </summary>
         public ReplySettings? ReplySettings { init; get; }
+        /// <summary>
+        /// Objects attached to the tweet
+        /// </summary>
         public Attachments Attachments { set; get; }
 
         [JsonIgnore]
--- TwitterSharp/Response/RUser/User.cs
+++ TwitterSharp/Response/RUser/User.cs
@@ -5,21 +5,60 @@ namespace TwitterSharp.Response.RUser
 {
     public class User : IEquatable<User>
     {
+        /// <summary>
+        /// Unique identifier of the user
+        /// </summary>
         public string Id { init; get; }
+        /// <summary>
+        /// Name shown on the user profile
+        /// </summary>
         public string Name { init; get; }
+        /// <summary>
+        /// Twitter handle
+        /// </summary>
         public string Username { init; get; }
 
         // OPTIONAL
 
+        /// <summary>
+        /// Creation date of the account
+        /// </summary>
         public DateTime? CreatedAt { init; get; }
+        /// <summary>
+        /// Profile description
+        /// </summary>
         public string Description { init; get; }
+        /// <summary>
+        /// Location specified on user profile
+        /// </summary>
         public string Location { init; get; }
+        /// <summary>
+        /// Identifier of the pinned tweet
+        /// </summary>
         public string PinnedTweetId { init; get; }
+        /// <summary>
+        /// URL to the picture profile
+        /// </summary>
         public string ProfileImageUrl { init; get; }
+        /// <summary>
+        /// Are the user tweet private
+        /// </summary>
         public bool? IsProtected { init; get; }
+        /// <summary>
+        /// URL specified on user profile
+        /// </summary>
         public string Url { init; get; }
+        /// <summary>
+        /// Information about special entities in user description
+        /// </summary>
         public Entities Entities { init; get; }
+        /// <summary>
+        /// Public metrics of the user
+        /// </summary>
         public UserPublicMetrics PublicMetrics { init; get; }
+        /// <summary>
+        /// If the user is a verified Twitter user
+        /// </summary>
         public bool? IsVerified { init; get; }
 
         public override bool Equals(object obj)
