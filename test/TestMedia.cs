6f19ea8c2477bc545460597591bf9dab2d1715e8
kirill <kirill@365scores.com>
2023-08-15 13:34:41 +0300

    media variants unit test

--- test/TestMedia.cs
+++ test/TestMedia.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TwitterSharp.Client;
 using TwitterSharp.Request.AdvancedSearch;
@@ -77,6 +78,32 @@ namespace TwitterSharp.UnitTests
             Assert.AreEqual(MediaType.Video, a.Attachments.Media[0].Type);
             Assert.IsNotNull(a.Attachments.Media[0].PreviewImageUrl);
             Assert.AreEqual("https://pbs.twimg.com/ext_tw_video_thumb/1237543944570847233/pu/img/kRBUlSd7M7ju_QK1.jpg", a.Attachments.Media[0].PreviewImageUrl);
+        }
+
+        [TestMethod]
+        public async Task GetTweetWithVideoMediaVariants()
+        {
+            var client = new TwitterClient(Environment.GetEnvironmentVariable("TWITTER_TOKEN"));
+            var answer = await client.GetTweetsAsync(new[] { "1688848776612691968" }, new TweetSearchOptions
+            {
+                TweetOptions = new[] { TweetOption.Attachments },
+                MediaOptions = new [] { MediaOption.Variants }
+            });
+            Assert.IsTrue(answer.Length == 1);
+            var a = answer[0];
+            Assert.IsNotNull(a.Attachments);
+            Assert.IsNotNull(a.Attachments.Media);
+            Assert.AreEqual(1, a.Attachments.Media.Length);
+            Assert.AreEqual("13_1688840150338428928", a.Attachments.Media[0].Key);
+            Assert.IsNotNull(a.Attachments.Media[0].Type);
+            Assert.AreEqual(MediaType.Video, a.Attachments.Media[0].Type);
+            Assert.IsNull(a.Attachments.Media[0].PreviewImageUrl);
+
+            Assert.IsNotNull(a.Attachments.Media[0].Variants);
+            Assert.IsTrue(a.Attachments.Media[0].Variants.Length > 0);
+            Assert.IsTrue(a.Attachments.Media[0].Variants.Any(v => v.ContentType == "video/mp4"));
+            Assert.IsTrue(a.Attachments.Media[0].Variants.Any(v => v.BitRate.HasValue));
+            Assert.IsTrue(a.Attachments.Media[0].Variants.All(v => string.IsNullOrWhiteSpace(v.Url) == false));
         }
     }
 }
